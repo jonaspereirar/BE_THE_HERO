275912d2948c0c408e0ac3ceeeba635023ac56ce
jonaspereirar <jonasprodrigues@gmail.com>
2020-03-24 23:50:52 +0000

    first commit: init Backend project

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+-Adicionar Nodemon
+-Adicionar ao GitHub
+-Funcionalidades
+.Login da ONG
+.Logout da ONG
+.Cadastro da ONG
+.Cadastrar novos casos
+.Deletar casos
+.Listar casos específicos de uma ONG
+.Listar todos os casos
+.Entrar em contacto com a ONG
---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.post('/users', (req, res) => {
+  const body = req.body;
+
+  return res.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Jonas Rodrigues'
+  })
+})
+
+module.exports = routes;
