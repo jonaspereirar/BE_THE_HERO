dba2d698ea1c5c9c89ba7a4c78a4a994b17931b4
jonaspereirar <jonasprodrigues@gmail.com>
2020-03-25 22:38:52 +0000

    feat. add.: CRUD incident controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,15 @@ const express = require('express');
 const crypto = require('crypto');
 
 const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes;
