f60ef22215e6fb1179ac9827b73f68b0b93b68b4
jonaspereirar <jonasprodrigues@gmail.com>
2020-03-25 23:08:49 +0000

    feat. add.: route list incidents by profile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const crypto = require('crypto');
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/profile', ProfileController.index)
+
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
 routes.delete('/incidents/:id', IncidentController.delete)
+
 
 module.exports = routes;
