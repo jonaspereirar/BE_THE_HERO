82b9b39287a0050442497121dc4cffce45d43ce2
jonaspereirar <jonasprodrigues@gmail.com>
2020-03-26 23:08:27 +0000

    feat. add.: install react-icons use login icon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css';
 
@@ -13,6 +14,15 @@ export default function Logon() {
 
         <form>
           <h1>Faça seu login</h1>
+
+          <input placeholder="Sua ID"/>
+          <button type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+
+          </a>
         </form>      
       </section>
 
