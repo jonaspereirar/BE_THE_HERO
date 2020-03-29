3eb882316a6203f6c36a0401bad50df1a2ae9d94
jonaspereirar <jonaspereirar@gmail.com>
2020-03-29 00:43:08 +0000

    feat. add.: route register of ONGS

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiLogIn } from 'react-icons/fi';
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
 
-import './styles.css';
+import './styles.css' 
 
 import logoImg from '../../assets/logo.svg'
-import heroesImg from '../../assets/heroes.png'
 
-export default function Logon() {
+export default function Register() {
   return (
-    <div className="logon-container">
-      <section className="form">
-        <img src={logoImg} alt="Be The Hero" />
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero"/>
+          
+          <h1>Cadastro</h1>
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoas e encontrem os casos da sua ONG.</p>
+
+          <Link className='back-link' to="/">
+            <FiArrowLeft size={16} color='#E02041' />
+            Efetuar login
+          </Link>
+        </section>
 
         <form>
+          <input placeholder='Nome da ONG'/>
+          <input placeholder='email'/>
+          <input placeholder='WhatsApp'/>
 
-          <input placeholder="Nome da ONG"/>
-            <input placeholder="email"/>
-            <input placeholder="Whadsapp"/>
-            <input placeholder="cidade"/>
-            <input placeholder="código Postal"/>
-          <button className="button" type="submit">Efetuar Inscrição</button>
+          <div className="input-group">
+            <input placeholder='Cidade'/>
+            <input placeholder='UF' style={{ width: 80 }} />
+          </div>
 
-          <Link to="/register">
-            <FiLogIn size={16} color="#E02041" />
-
-
-          </Link>
-        </form>      
-      </section>
-
+          <button type='submit' className="button">Cadastrar</button>
+        </form>
+      </div>
     </div>
   )
 }
